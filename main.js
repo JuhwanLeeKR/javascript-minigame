8775d521f064169ee157fb0d6522d75eb9dd4391
Juhwan <dev.juhwanlee@gmail.com>
2021-11-27 19:44:07 +0900

    ♻️ Refactoring code

--- main.js
+++ main.js
@@ -11,22 +11,23 @@ while (!number) {
   alert('숫자를 입력해 주세요.');
   number = parseInt(prompt('몇 명이 참가하나요?'));
 }
+// if (number) {} 로 전체를 감싸줘서 실행하지 않게 하는 방법 또한 있다.
 
 const onClickButton = () => {
   // 제시어가 비어 있거나 입력한 단어가 끝말잇기 규칙에 맞는가?
-  if (!word || word[word.length - 1] === newWord[0]) {
-    // 세 글자가 아니면 경고창을 띄운다.
-    if (newWord.length !== 3) {
-      alert('세 글자만 입력해 주세요.');
+  if (
+    (!word &&
+      newWord.length ===
+        3) /* 설정을 해주지 않으면 첫 단어가 3글자가 아닌 것도 가능하다 */ ||
+    (word[word.length - 1] === newWord[0] && newWord.length === 3)
+  ) {
+    word = newWord; // 입력한 단어가 제시어가 된다.
+    $word.textContent = word;
+    const order = parseInt($order.textContent); // 현재 순서
+    if (order + 1 > number) {
+      $order.textContent = 1;
     } else {
-      word = newWord; // 입력한 단어가 제시어가 된다.
-      $word.textContent = word;
-      const order = parseInt($order.textContent); // 현재 순서
-      if (order + 1 > number) {
-        $order.textContent = 1;
-      } else {
-        $order.textContent = order + 1;
-      }
+      $order.textContent = order + 1;
     }
   } else {
     alert('올바르지 않은 단어 입니다.');
