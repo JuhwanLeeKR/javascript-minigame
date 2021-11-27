196baf188c5b89529b5224857b3e5c3a976df1e2
Juhwan <dev.juhwanlee@gmail.com>
2021-11-27 18:50:43 +0900

    🔨 Add ability to limit to three characters

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const number = parseInt(prompt('몇 명이 참가하나요?'));
+let number = parseInt(prompt('몇 명이 참가하나요?'));
 const $button = document.querySelector('button');
 const $input = document.querySelector('input');
 const $word = document.querySelector('#word');
@@ -6,20 +6,29 @@ const $order = document.querySelector('#order');
 let word; // 제시어
 let newWord; // 새로 입력한 단어
 
+// prompt 창을 끄거나 잘못 입력했을 때
+while (!number) {
+  alert('숫자를 입력해 주세요.');
+  number = parseInt(prompt('몇 명이 참가하나요?'));
+}
+
 const onClickButton = () => {
+  // 제시어가 비어 있거나 입력한 단어가 끝말잇기 규칙에 맞는가?
   if (!word || word[word.length - 1] === newWord[0]) {
-    // 제시어가 비어 있는가?
-    // 비어 있다.
-    word = newWord; // 입력한 단어가 제시어가 된다.
-    $word.textContent = word;
-    const order = parseInt($order.textContent); // 현재 순서
-    if (order + 1 > number) {
-      $order.textContent = 1;
+    // 세 글자가 아니면 경고창을 띄운다.
+    if (newWord.length !== 3) {
+      alert('세 글자만 입력해 주세요.');
     } else {
-      $order.textContent = order + 1;
+      word = newWord; // 입력한 단어가 제시어가 된다.
+      $word.textContent = word;
+      const order = parseInt($order.textContent); // 현재 순서
+      if (order + 1 > number) {
+        $order.textContent = 1;
+      } else {
+        $order.textContent = order + 1;
+      }
     }
   } else {
-    // 비어 있지 않다.
     alert('올바르지 않은 단어 입니다.');
   }
   $input.value = '';
