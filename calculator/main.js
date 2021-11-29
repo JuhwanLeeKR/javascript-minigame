7449b107e4fc7b1a0b891e526c1a362accc01eb1
Juhwan <dev.juhwanlee@gmail.com>
2021-11-29 21:36:14 +0900

    🐛 Fix bug and add How to remove nested if statement

--- calculator/main.js
+++ calculator/main.js
@@ -3,9 +3,19 @@ let operator = '';
 let numTwo = '';
 const $operator = document.querySelector('#operator');
 const $result = document.querySelector('#result');
+/* if 문 중첩을 제거하는 방법
+1. if 문 다음에 나오는 공통된 절차를 각 분기점 내부에 넣는다.
+2. 분기점에서 짧은 절차부터 실행하게 if 문을 작성한다.
+3. 짧은 절차가 끝나면 return(함수 내부의 경우)이나 break(for 문 내부의 경우)로 중단한다.
+4. else를 제거한다(이때 중첩 하나가 제거된다).
+5. 다음 중첩된 분기점이 나오면 1~4의 과정을 반복한다.
+*/
 const onClickNumber = (event) => {
   if (operator) {
     // 비어있지 않다
+    if (!numTwo) {
+      $result.value = '';
+    }
     numTwo += event.target.textContent;
   } else {
     // 비어있다
