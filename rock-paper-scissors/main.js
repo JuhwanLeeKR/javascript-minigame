5b3ceae747d7296ded65ab6017271163ab744b36
Juhwan <dev.juhwanlee@gmail.com>
2021-12-16 02:47:35 +0900

    📝 Set up basic

---
+++ rock-paper-scissors/main.js
@@ -0,0 +1,8 @@
+const $computer = document.querySelector('#computer');
+const $score = document.querySelector('#score');
+const $rock = document.querySelector('#rock');
+const $scissors = document.querySelector('#scissors');
+const $paper = document.querySelector('#paper');
+const IMG_URL = './rsp.png';
+$computer.style.background = `url(${IMG_URL}) 0 0`;
+$computer.style.backgroundSize = 'auto 200px';
