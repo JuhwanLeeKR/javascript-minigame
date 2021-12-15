2183377cd504077c0275eb5ec41575632b894167
Juhwan <dev.juhwanlee@gmail.com>
2021-12-16 03:16:44 +0900

    🔨 Add function by setTimeout()

--- rock-scissors-paper/main.js
+++ rock-scissors-paper/main.js
@@ -3,6 +3,29 @@ const $score = document.querySelector('#score');
 const $rock = document.querySelector('#rock');
 const $scissors = document.querySelector('#scissors');
 const $paper = document.querySelector('#paper');
-const IMG_URL = './rsp.png';
-$computer.style.background = `url(${IMG_URL}) 0 0`;
+const IMG_URL = './assets/rsp.png';
+$computer.style.background = `url(${IMG_URL}) 0 0`; // 가위
 $computer.style.backgroundSize = 'auto 200px';
+
+const rspX = {
+  scissors: '0', // 가위
+  rock: '-220px', // 바위
+  paper: '-440px', // 보
+};
+
+let computerChoice = 'scissors';
+const changeComputerHand = () => {
+  if (computerChoice === 'rock') {
+    // 가위
+    computerChoice = 'rock'; // 화면을 바꿔주면 data 또한 바꿔야 한다.
+  } else if (computerChoice === 'scissors') {
+    // 바위
+    computerChoice = 'paper';
+  } else if (computerChoice === 'paper') {
+    // 보
+    computerChoice = 'scissors';
+  }
+  $computer.style.background = `url(${IMG_URL}) ${rspX[computerChoice]} 0`;
+  $computer.style.backgroundSize = 'auto 200px';
+};
+setInterval(changeComputerHand, 50);
