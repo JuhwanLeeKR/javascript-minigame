1a324b24557ed9ff91541dd1f59c79f87525d566
Juhwan <dev.juhwanlee@gmail.com>
2022-01-04 22:32:31 +0900

    🔨 Add function

--- reaction-speed-test/main.js
+++ reaction-speed-test/main.js
@@ -1,4 +1,15 @@
 const $screen = document.querySelector('#screen');
 const $result = document.querySelector('#result');
 
-$screen.className; // waiting
+$screen.addEventListener('click', function () {
+  if ($screen.classList.contains('waiting')) {
+    // 대기 화면
+    $screen.classList.remove('waiting');
+    $screen.classList.add('ready');
+    $screen.textContent = '초록색이 되면 클릭하세요';
+  } else if ($screen.classList.contains('ready')) {
+    // 준비 화면
+  } else if ($screen.classList.contains('now')) {
+    // 클릭 화면
+  }
+});
